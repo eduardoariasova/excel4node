349e9530c3568b2a3b445ca69350231a13cf39bf
Eduardo Arias <luisearias0830@gmail.com>
2023-01-22 19:39:48 -0500

    excel4node configurado con columnas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const xl = require('excel4node');
+const path = require('path');
 // express //////////
 const app = express();
 app.use(express.json());
@@ -10,6 +12,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 // Inicialización ///////////////////////////////////////
+
+//FECHA
+let date = new Date();
+let fechaDia    = date.getUTCDate();
+let fechaMes    = (date.getUTCMonth()) + 1; 
+let fechaAño    = date.getUTCFullYear();
+let fechaHora   = date.getUTCHours();
+let fechaMinuto = date.getUTCMinutes();
 
 
 
@@ -74,12 +84,57 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.post("/descargar-excel", function(req, res){
+app.get("/descargar-excel", function(req, res){
     
-    
+    // Configurar excel4node
+    // Create a new instance of a Workbook class
+    var wb = new xl.Workbook();
+    let nombreArchivo = "todosUsuarios" + fechaDia + "_" + fechaMes + "_" + fechaAño + ".";
+    var ws = wb.addWorksheet(nombreArchivo);
+
+    // Crear estilos
+    var cualColumnaEstilo = wb.createStyle({
+        font: {
+            name: 'Arial',
+            color: '#000000',
+            size: 12,
+            bold: true,
+        }
+    });
+
+    var contenidoEstilo = wb.createStyle({
+        font: {
+            name: 'Arial',
+            color: '#494949',
+            size: 11,
+        }
+    });
 
 
+    //Nombres de las columnas
+    ws.cell(1, 1).string("Nombre").style(cualColumnaEstilo);
+    ws.cell(1, 2).string("Apellido").style(cualColumnaEstilo);
+    ws.cell(1, 3).string("Edad").style(cualColumnaEstilo);
+    ws.cell(1, 4).string("ID").style(cualColumnaEstilo);
+    ws.cell(1, 5).string("Teléfono").style(cualColumnaEstilo);
+    ws.cell(1, 6).string("Correo").style(cualColumnaEstilo);
 
+
+    //Ruta del archivo
+    const pathExcel = path.join(__dirname, 'excel', nombreArchivo + '.xlsx');
+
+    //Escribir o guardar
+    wb.write(pathExcel, function(err, stats){
+        if(err) console.log(err);
+        else{
+
+            // Crear función y descargar archivo
+            function downloadFile(){res.download(pathExcel);}
+            downloadFile();
+
+            console.log("Archivo descargado correctamente");
+        }
+    });
 });
 
 
